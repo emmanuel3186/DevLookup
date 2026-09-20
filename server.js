d8feca12d6b3a7918fdcee571cf83fbe3cd35392
Emmanuel Folorunsho <emmanuelfolorunsho958@gmail.com>
2026-09-20 21:33:13 +0100

    fix: export express app for serverless

--- server.js
+++ server.js
@@ -35,4 +35,8 @@ app.get('/api/dev/username', async (req, res) => {
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 module.exports = app;
